322a74917b6f50d34591b500b4a9fb3784ce6e53
Shubhangee Sunil Borse <157007786+shubhangi-patil0297@users.noreply.github.com>
2024-04-28 23:13:05 +0530

    Add files via upload

---
+++ C/Basic02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("Hello World\n");
+    printf("Number: %d\n", 100);
+    printf("Float: %f\n", 100.0);
+    printf("Double: %lf\n", 100.0);
+    printf("Character: %c\n", 'A');
+    
+    return 0;
+}
