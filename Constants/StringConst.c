5b881be6b8df07173bbfd0d2aa398fccdb7ae943
Shubhangee Sunil Borse <157007786+shubhangi-patil0297@users.noreply.github.com>
2024-05-08 17:20:08 +0530

    Add files via upload

---
+++ Constants/StringConst.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#define PI 3.14159
+#define MAX_LENGTH 100
+
+int main() {
+    char stringConstant[] = "Hello, World!";
+    printf("String Constant: %s\n", stringConstant);
+
+    printf("Value of PI: %.5f\n", PI);
+    printf("Maximum Length: %d\n", MAX_LENGTH);
+
+    return 0;
+}
---
+++ Constants/basic01.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+    char charConstant = 'A';
+    printf("Character Constant: %c\n", charConstant);
+
+    // Integer Constants
+    int integerConstant = 42;
+    printf("Integer Constant: %d\n", integerConstant);
+
+    // Floating-point Constants
+    float floatConstant = 3.14159;
+    double doubleConstant = 2.71828;
+    printf("Float Constant: %.5f\n", floatConstant);
+    printf("Double Constant: %.5lf\n", doubleConstant);
+
+    return 0;
+}
