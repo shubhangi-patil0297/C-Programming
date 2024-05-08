cd333af0b53b45b03480aa966918d0d03766563a
Shubhangee Sunil Borse <157007786+shubhangi-patil0297@users.noreply.github.com>
2024-05-08 17:10:33 +0530

    Add files via upload

---
+++ Variable/userinputVariable.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+  //initialize vars
+  char name[20]; 
+  int age;
+  float height;
+
+  
+  printf("enter your name \n ");
+  scanf("%s",&name);
+  printf("enter your age \n");
+  scanf("%d",&age);
+  printf("enter your height \n");
+  scanf("%f",&height);
+  
+
+  
+  printf("\n --------output---------- \n");
+  printf("Name : %s\n",name);
+  printf("Age : %d\n",age);
+  printf("Height : %f\n",height);
+  
+}
