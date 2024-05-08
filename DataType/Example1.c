ada7e7ddf15381f5d0dfff3034a20cc7e96030f8
Shubhangee Sunil Borse <157007786+shubhangi-patil0297@users.noreply.github.com>
2024-05-08 17:14:15 +0530

    Add files via upload

---
+++ DataType/Example1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+int main()
+{
+    // integer 
+    int integerNumber = 100;
+    printf("Integer Number: %d\n", integerNumber);
+    
+    // float datatype
+    float floatNumber = 100.0;
+    printf("Float Number: %f\n",floatNumber);
+    
+    // double datatype
+    double doubleNumber = 100.0;
+    printf("Double Number: %lf\n", doubleNumber);
+    
+    // character datatype
+    char character = 'A';
+    printf("Character: %c\n", character);
+    
+    return 0;
+}
